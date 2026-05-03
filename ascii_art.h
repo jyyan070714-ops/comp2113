8beccf571934c9938f092c78f2b371fe6141fe37
Sun-xiaosong <sunxiaosong070310@163.com>
2026-05-03 23:23:52 +0800

    Update ascii_art.h

--- ascii_art.h
+++ ascii_art.h
@@ -2,8 +2,18 @@
 #define ASCII_ART_H
 
 #include <string>
-
+/**
+ * @brief Prints an ASCII art representation of the player character to the console.
+ * @param none
+ * @return void
+ */
 void printPlayerArt();
+/**
+ * @brief Prints an ASCII art representation of an enemy based on its name.
+ *        Different enemies (Small Slime, Goblin Marauder, Goblin Boss) have distinct art.
+ * @param enemyName The name of the enemy, used to select the appropriate ASCII art.
+ * @return void
+ */
 void printEnemyArt(const std::string& enemyName);
 
 #endif
