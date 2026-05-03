0c38d0bf64b13fd1db0251dcfbe5fa24b94cb224
Sun-xiaosong <sunxiaosong070310@163.com>
2026-05-03 23:22:51 +0800

    Update ascii_art.cpp

--- ascii_art.cpp
+++ ascii_art.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 
 using namespace std;
-//player
+/**
+ * @brief Prints an ASCII art representation of the player character to the console.
+ * @param none
+ * @return void
+ */
 void printPlayerArt() {
     cout << R"(
    _______
@@ -12,7 +16,13 @@ void printPlayerArt() {
   |_______|
 )";
 }
-//small slime
+/**
+ * @brief Prints an ASCII art representation of an enemy based on its name.
+ *        Different enemies have different art styles.
+ * @param enemyName The name of the enemy (e.g., "Small Slime", "Goblin Marauder", "Goblin Boss").
+ * @return void
+ */
+
 void printEnemyArt(const string& enemyName) {
     if (enemyName == "Small Slime") {
         cout << R"(
