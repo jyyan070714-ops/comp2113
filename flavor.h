e99dc346d6419f6b1e7683f3ad148cec6f8d1536
Sun-xiaosong <sunxiaosong070310@163.com>
2026-05-03 23:21:27 +0800

    Update flavor.h

--- flavor.h
+++ flavor.h
@@ -2,7 +2,12 @@
 #define FLAVOR_H
 
 #include "common.h"
-
+/**
+ * @brief Displays a random flavorful description text based on the given node type.
+ *        The text is printed to the console with decorative asterisks.
+ * @param type The type of node (FIGHT, ELITE, BOSS, EVENT, REST) to determine which flavor pool to use.
+ * @return void
+ */
 void showFlavorText(NodeType type);
 
 #endif // FLAVOR_H
